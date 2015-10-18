fd4b670ed8726a7cc6f5fb97356ba6652cdda6e0
Loan Vulliard <loan.vulliard@insa-lyon.fr>
2015-10-18 14:34:39 +0200

    Initial commit

---
+++ GuessBS.py
@@ -0,0 +1,11 @@
+#****************************************************************************
+#
+#                                   GuessBS
+#
+#****************************************************************************
+
+print "GuessBS - Reverse Quizz - v0.1"
+print "Let's find someone from the BS dept. by answering a few questions!"
+question_count = 1
+print 'Question number #%d:' % (question_count,)
+ans = raw_input("Yes (Y) / No (N) / Maybe (M): ")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # git_GuessBS
 Small "reverse quizz" game with simple machine learning and network communication
+First, you need to chose someone from the BS dept., then this software will try to guess who this is. To achieve that, it will ask you questions, and you will have to by Y (for yes), N (for no), or M (for maybe, if you are not sure or do not know the correct answer).
+- Changelog - 
+v0.1 _ Implementing basic game features
