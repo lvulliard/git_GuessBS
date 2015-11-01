a2183020092d45c3458f014cca71a544600cfb1c
Loan Vulliard <loan.vulliard@insa-lyon.fr>
2015-11-01 14:26:44 +0100

    Computed the score data after a run

--- GuessBS.py
+++ GuessBS.py
@@ -19,8 +19,8 @@ ans_dict = {}
 # ID of the current question
 question_id = random.randint(0, len(questions)-1)
 	
-# Ask 3 questions
-for i in xrange(3):
+# Ask 5 questions
+for i in xrange(5):
 	# If all questions have already been asked, stop
 	if question_count > len(questions):
 		print "\nThat's all the questions I know, sorry..."
@@ -46,3 +46,51 @@ for i in xrange(3):
 
 	# One more question has been asked
 	question_count += 1
+
+ans = raw_input("\nWho was he ? (Please do not make any orthographic mistake...)\n")
+
+#score_files = open('rsc/score')
+# Read the score dictionary
+# Key = "FirstName LastName"
+# Value = {QuestionID : [Score, cardinal]}
+# Complexity for n characters, p questions answered k times in average :
+# Comp = O(n*p*k)
+# For instance :
+score_dict = {"Hubert Charles" : {0 : [0.95, 4], 1 : [0.96, 3], 2 : [0.5, 2]}}
+
+if ans in score_dict:
+	# If it is not the first time the character have been chosen by the user
+	# Store the answer for each question in his dictionary
+    for question_id in ans_dict.keys():
+    	print ans_dict[question_id] 
+    	if ans_dict[question_id] == 'M':
+    		score_val = 0
+    	if ans_dict[question_id] == 'Y':
+    		score_val = 1	 
+    	if ans_dict[question_id] == 'N':
+    		score_val = -1
+    	if question_id in score_dict[ans] :
+    		# Not the first time this question is aked about this character
+    		# Do a weighted average
+    		old_score = (score_dict[ans])[question_id]
+    		new_card = old_score[1] + 1
+    		(score_dict[ans])[question_id] = [ (score_val+(old_score[0]*old_score[1]))/new_card, new_card]
+    	else :
+    		# First time this question is answered about that character
+	    	(score_dict[ans])[question_id] = [score_val, 1]
+else:
+	# If it is the first time
+	# Create a dictionary for this character
+    score_dict[ans] = {}
+    # Store the answer for each question in his dictionary
+    for question_id in ans_dict.keys():
+    	if ans_dict[question_id] == 'M':
+    		score_val = 0
+    	if ans_dict[question_id] == 'Y':
+    		score_val = 1	 
+    	if ans_dict[question_id] == 'N':
+    		score_val = -1
+    	(score_dict[ans])[question_id] = [score_val, 1]
+
+# Write the score dictionary
+print score_dict
