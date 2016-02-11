2b51156dff976d50ad465d9a86f1a4e80bf7d17b
Xius <alexispiat77420@msn.com>
2016-02-11 16:19:24 +0100

    commented code

--- Projetcli.py
+++ Projetcli.py
@@ -1,13 +1,18 @@
 from socket import *
 
+
 # Client socket creation
 s = socket(AF_INET,SOCK_STREAM)
 s.connect(('127.0.0.1',8000))
 print 'Connection established.'
+
 name = s.recv(1024)
 print '\n', name
 name2 = data = raw_input()
 s.sendto(name2,('',51432))
+
+
+#reception and sending with the serveur: serveur send question and the player send the answer
 question = ''
 
 # Does the user asked to exit the game ?
@@ -15,13 +20,14 @@ user_exit = 0
 
 while not user_exit: # While the game continues
 	resp = '' # No answer given by the user yet
+
 	c = 0
 	question = s.recv(1024)
 	if question == 'end':
 		break
-	else : 
 		while (resp.upper() not in ['Y', 'N', 'M', 'EXIT']):
 			if c == 0:
+
 				print '\n', question
 		 		resp = raw_input("Yes (Y) / No (N) / Maybe (M): ")
 		 		c +=1
@@ -35,10 +41,17 @@ while not user_exit: # While the game continues
 		if not user_exit :
 			s.sendto(resp,('',51432))
 
-if not user_exit :
+#End of Game
+
+if not user_exit :       
+
+	#Serveur send his answer
 	prop = s.recv(1024)
 	print '\n',prop 
 	rep = raw_input("Yes (Y) / No (N): ")
+
+
+	#if it's the good answer, serveur end the game 
 	if rep == 'Y' or rep == 'y':
 		s.sendto(rep,('',51432))
 		rep2 = s.recv(1024)
